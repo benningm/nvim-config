aad0ee18f1aaa0bea9bc76400e4783185691a71a
Markus Benning <ich@markusbenning.de>
2021-09-26 20:07:33 +0200

    Use new setup for nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,7 +42,6 @@ return require('packer').startup(function()
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
     config = function()
-      require'nvim-tree'.setup {}
       require("plugins.nvim-tree")
     end
   }
