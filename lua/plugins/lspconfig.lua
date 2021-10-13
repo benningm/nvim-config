d2236599d2c32552d89a63c97940521f4a69c5af
Markus Benning <markus.benning@atos.net>
2021-10-13 09:01:21 +0200

    Add lsp config for puppet

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,20 @@
 local nvim_lsp = require('lspconfig')
+local util = require 'lspconfig/util'
 
 nvim_lsp['solargraph'].setup{
   cmd = { "docker-compose", "exec", "-T", "web", "solargraph", "stdio" }
   -- on_attach=require'completion'.on_attach
+}
+
+nvim_lsp['puppet'].setup {
+  cmd = { 'puppet-languageserver', '--stdio' },
+  filetypes = { 'puppet' },
+  root_dir = function(fname)
+    local root_files = {
+      "manifests",
+      "metadata.json",
+      ".git"
+    }
+    return util.root_pattern(unpack(root_files))(fname) or util.path.dirname(fname)
+  end,
 }
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -146,18 +146,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-treesitter
-time([[Config for nvim-treesitter]], true)
-require("plugins.nvim-treesitter")
-time([[Config for nvim-treesitter]], false)
--- Config for: galaxyline.nvim
-time([[Config for galaxyline.nvim]], true)
-try_loadstring("\27LJ\2\n2\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\23plugins.galaxyline\frequire\0", "config", "galaxyline.nvim")
-time([[Config for galaxyline.nvim]], false)
--- Config for: completion-nvim
-time([[Config for completion-nvim]], true)
-require("plugins.completion")
-time([[Config for completion-nvim]], false)
 -- Config for: nvim-lspconfig
 time([[Config for nvim-lspconfig]], true)
 require("plugins.lspconfig")
@@ -170,10 +158,22 @@ time([[Config for gitsigns.nvim]], false)
 time([[Config for nvim-tree.lua]], true)
 try_loadstring("\27LJ\2\n1\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\22plugins.nvim-tree\frequire\0", "config", "nvim-tree.lua")
 time([[Config for nvim-tree.lua]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+require("plugins.nvim-treesitter")
+time([[Config for nvim-treesitter]], false)
 -- Config for: nvim-lastplace
 time([[Config for nvim-lastplace]], true)
 require("plugins.lastplace")
 time([[Config for nvim-lastplace]], false)
+-- Config for: completion-nvim
+time([[Config for completion-nvim]], true)
+require("plugins.completion")
+time([[Config for completion-nvim]], false)
+-- Config for: galaxyline.nvim
+time([[Config for galaxyline.nvim]], true)
+try_loadstring("\27LJ\2\n2\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\23plugins.galaxyline\frequire\0", "config", "galaxyline.nvim")
+time([[Config for galaxyline.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd nvim-ts-rainbow ]]
